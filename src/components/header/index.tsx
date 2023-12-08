cf76da4f14c5a3b106c846143f6b6e321a333343
João <joaovictor.ramos34@gmail.com>
2023-12-08 16:44:14 -0300

    feat(context cart): criando o contexto do carrinho

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,12 @@
 import { Link } from 'react-router-dom'
 import { FiShoppingCart } from 'react-icons/fi'
 
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/CartContext';
+
 export function Header() {
+  const { cartAmount } = useContext(CartContext);
+
   return (
     <header className='w-full px-1 bg-slate-600'>
       <nav className='w-full max-w-7xl h-20 flex items-center justify-between px-8 mx-auto '>
@@ -10,9 +15,11 @@ export function Header() {
         </Link>
         <Link to='/cart' className='relative text-white'>
           <FiShoppingCart size={26} />
-          <span className='absolute -right-5 -top-3 px-2.5 bg-sky-500 w-6 h-6 flex items-center justify-center rounded-full font-bold'>
-            2
-          </span>
+          {
+            cartAmount > 0 && (
+              <span className='absolute -right-5 -top-3 px-2.5 bg-sky-500 w-6 h-6 flex items-center justify-center rounded-full font-bold'> {cartAmount}</span>
+            )
+          }
         </Link>
       </nav>
     </header>
